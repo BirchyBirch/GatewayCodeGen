279a3b04cb5a02417f5a42268adaca8429184e81
Chris Burchsted <chris.burchsted+github@gmail.com>
2016-08-20 17:38:34 -0500

    fixing .net version in build script

--- build.ps1
+++ build.ps1
@@ -17,7 +17,7 @@ Import-Module $psakeModule
 # you can write statements in multiple lines using `
 Invoke-psake -buildFile .\Build\default.ps1 `
 			 -taskList compileWithCover `
-			 -framework 4.5/2 `
+			 -framework 4.5.2 `
 		     -properties @{ 
 				 "buildConfiguration" = "Release"
 				 "buildPlatform" = "Any CPU"} `
